b632d82926e3755a6bab7126aa8ac385c394a1c2
Dan095ss <daniil095ss@gmail.com>
2020-05-07 17:32:06 +1000

    Profile + Chat V2

--- README.md
+++
@@ -1,2 +0,0 @@
-# chat-neon
- Neon design chat system
---
+++ main.py
@@ -0,0 +1,55 @@
+import pusher
+from flask import Blueprint, render_template, request, jsonify
+from flask_login import login_required, current_user
+from .models import Message
+from . import db
+
+
+
+main = Blueprint('main', __name__)
+
+pusher_client = pusher.Pusher(
+  app_id='996676',
+  key='fc2a090ac7e3a54357b1',
+  secret='6c148af6fdf7524d0619',
+  cluster='ap3',
+  ssl=True
+)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+
+@main.route('/chat')
+def chat():
+    messages = Message.query.all()
+
+    return render_template('chat.html', messages=messages, name=current_user.name)
+
+
+@main.route('/message', methods=['POST'])
+def message():
+    try:
+
+        username = request.form.get('username')
+        message = request.form.get('message')
+
+        new_message = Message(username=username, message=message)
+        db.session.add(new_message)
+        db.session.commit()
+
+        pusher_client.trigger('chat-channel', 'new-message', {'username': username, 'message': message})
+
+        return jsonify({'result': 'success'})
+
+    except:
+
+        return jsonify({'result': 'failure'})
+
